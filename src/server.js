3d719d04666128a9bf198d612d0d2a2243ee1748
kayas1 <wogur1833@naver.com>
2023-08-27 19:07:12 +0900

    3-3

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-app.get("/", ()=>console.log("Somebody is trying to go home"));
 
-const handleListening = () =>console.log(`Server is listening on port http://localhost:${PORT}`);
+const handleHome = (req, res)=> {
+    return res.send("I still love you.");
+}
+const handleLogin =(req, res)=>{
+    return res.send("Login here");
+}
 
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+const handleListening = (req, res) =>console.log(`server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
