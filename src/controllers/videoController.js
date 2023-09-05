a326b229b88b3121e548786ecd8f19aa3337ddf5
kayas1 <wogur1833@naver.com>
2023-09-06 02:09:13 +0900

    5-10

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,8 @@
-export const trending = (req,res) => res.render("home", {pageTitle: "Home"});
+export const trending = (req,res) => {
+    const videos=[{title:"First video",rating:5,comments:3,createAt:"5mins ago",views:352,id:1},
+    {title:"Second video",rating:4,comments:33,createAt:"51mins ago",views:352,id:2},
+    {title:"Third video",rating:2,comments:223,createAt:"59mins ago",views:3522,id:3}]
+    res.render("home", {pageTitle: "Home", videos});}
 export const see = (req,res)=> res.render("watch", {pageTitle: "Watch"});
 export const edit=(req,res)=>res.render("edit", {pageTitle:"Edit"});
 export const search=(req,res)=>res.send("Search");
