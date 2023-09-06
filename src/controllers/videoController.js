e8898810c335595f5582fe606cf5092e85a3a274
kayas1 <wogur1833@naver.com>
2023-09-07 03:25:52 +0900

    6-3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,23 @@
-export const trending = (req,res) => {
-    const videos=[{title:"First video",rating:5,comments:3,createAt:"5mins ago",views:352,id:1},
-    {title:"Second video",rating:4,comments:33,createAt:"51mins ago",views:352,id:2},
+const videos=[{title:"First video",rating:5,comments:3,createAt:"5mins ago",views:352,id:1},
+    {title:"Second video",rating:4,comments:33,createAt:"51mins ago",views:1,id:2},
     {title:"Third video",rating:2,comments:223,createAt:"59mins ago",views:3522,id:3}]
+
+export const trending = (req,res) => {
     res.render("home", {pageTitle: "Home", videos});}
-export const see = (req,res)=> res.render("watch", {pageTitle: "Watch"});
-export const edit=(req,res)=>res.render("edit", {pageTitle:"Edit"});
-export const search=(req,res)=>res.send("Search");
-export const upload =(req,res)=>res.send("Upload");
-export const deleteVideo=(req,res)=>res.send("Delete Video");
+export const watch = (req,res)=> {
+    const {id}= req.params;
+    const video=videos[id-1];
+    return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+}
+
+export const getEdit=(req,res)=>{
+    const {id}= req.params;
+    const video=videos[id-1];
+    res.render("edit", {pageTitle:`Editing: ${video.title}`, video}) ;
+}
+export const postEdit=(req,res)=>{
+    const {id}= req.params;
+    const {title}=req.body;
+    videos[id-1].title=title;
+    return res.redirect(`/videos/${id}`);
+}
