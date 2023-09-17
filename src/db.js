ac39057e20a9a72929e3d56be9367f77244ead0d
kayas1 <wogur1833@naver.com>
2023-09-17 16:49:51 +0900

    7-18

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.connect(process.env.DB_URL);
 
 const db=mongoose.connection;
 
