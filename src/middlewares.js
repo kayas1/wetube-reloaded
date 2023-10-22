36807a4fd4b279a36429948e300ccae8cc66c88d
kayas1 <wogur1833@naver.com>
2023-10-23 04:27:29 +0900

    bug fixing

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,14 +2,16 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "@aws-sdk/client-s3";
 
-const s3= new aws.S3({credentials:{
+const s3= new aws.S3({
+    credentials:{
     accessKeyId:process.env.AWS_ID,secretAccessKey:process.env.AWS_SECERT,
-}})
+    },
+});
 
 const multerUploader = multerS3({
     s3:s3,bucket:'wetube-reloaded-kayas1',
     acl:"public-read",
-})
+});
 
 export const localsMiddleware = (req,res,next)=>{
     res.locals.loggedIn= Boolean(req.session.loggedIn);
