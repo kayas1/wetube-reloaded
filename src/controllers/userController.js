c4d342168f8067d7d53bbbed6e69483ba61b11cc
kayas1 <wogur1833@naver.com>
2023-10-22 23:42:32 +0900

    bug fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,7 +47,7 @@ export const postLogin=async (req,res)=>{
 export const startGithubLogin=(req,res)=>{
     const baseUrl="https://github.com/login/oauth/authorize";
     const config = {
-        client_id:process.env.GH_CLIENT,
+        client_id:process.env.GH_Client,
         allow_signup:false,
         scope:"read:user user:email",
     };
@@ -59,7 +59,7 @@ export const startGithubLogin=(req,res)=>{
 export const finishGithubLogin = async(req,res)=>{
     const baseUrl="https://github.com/login/oauth/access_token";
     const config={
-        client_id:process.env.GH_CLIENT,
+        client_id:process.env.GH_Client,
         client_secret:process.env.GH_SECRET,
         code:req.query.code,
     };
